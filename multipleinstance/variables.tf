78af0c98c3f46f44b31df1db197882f1e7dd1ac2
deesnest <87329773+deesnest@users.noreply.github.com>
2021-09-21 14:08:37 +0530

    Create variables.tf

---
+++ multipleinstance/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+   default = "ami-015823f149dddbd48"
+}
+
+variable "instance_count" {
+  default = "<MENTION-NUMBER OF INSTANCES REQUIRED"
+}
+
+variable "instance_type" {
+  default = "<MENTION INSTANCE-TYPE>"
+}
+
+variable "aws_region" {
+  default = "<MENTION REGION HERE>"
+}
