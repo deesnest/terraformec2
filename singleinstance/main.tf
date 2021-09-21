a2475df6c7468e78f96f673348295f103959d1ed
deesnest <87329773+deesnest@users.noreply.github.com>
2021-09-21 14:04:07 +0530

    Update main.tf

--- singleinstance/main.tf
+++ singleinstance/main.tf
@@ -1,7 +1,6 @@
-resource "aws_instance" "myec"  {
+resource "aws_instance" "<INSTANCE-NAME HERE>"  {
 ami = "ami-015823f149dddbd48"
-instance_type = "t2.medium"
-
+instance_type = "<MENTION-INSTANCE-TYPE HERE"
 tags = {
 Name = "terracentos"
 }
